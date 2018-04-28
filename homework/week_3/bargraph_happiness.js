ddda3597d13448e3791ec2d5b9d929ba483e5e53
Jacob Jasper <jacob_jasper@hotmail.com>
2018-04-28 22:04:42 +0200

    week_3.2

--- homework/week_3/bargraph_happiness.js
+++ homework/week_3/bargraph_happiness.js
@@ -1,13 +1,107 @@
-d3.json("2016_happiness_westerneurope.json", function(data) {
-  d3.select("head").append("title").text("Happiness score 1 till 10 country's Western Europe");
-  d3.select("body").append("h1").text("Jacob Jasper, 10650385, Building your first visualization in D3.js");
-  d3.select("body").append("p").text(" The World Happiness Report is a landmark survey of the state of global \
-  happiness. The first report was published in 2012, the second in 2013, the third in 2015, and the fourth in \
-  the 2016 Update. The World Happiness 2017, which ranks 155 countries by their happiness levels, was released \
-  at the United Nations at an event celebrating International Day of Happiness on March 20th. The report continues \
-  to gain global recognition as governments, organizations and civil society increasingly use happiness indicators \
-  to inform their policy-making decisions. Leading experts across fields – economics, psychology, survey analysis, \
-  national statistics, health, public policy and more – describe how measurements of well-being can be used effectively \
-  to assess the progress of nations. The reports review the state of happiness in the world today and show how the new \
-  science of happiness explains personal and national variations in happiness. The bargraph here shows the data of 2016.");
+d3.json("2016_hardcoded.json", function(data){
+
+  // creating information elements of barchart
+  d3.select("head")
+    .append("title")
+    .text("Happiness score 1 till 10 country's Western Europe");
+
+  d3.select("body")
+    .append("h1")
+    .text("Jacob Jasper, 10650385, Building your first visualization in D3.js");
+
+  d3.select("body")
+    .append("p")
+    .html("The bargraph here shows the happiness scores of West-European \
+    country's of 2016. The data was retrieved from \
+    <a href='https://www.kaggle.com/datasets?sortBy=hottest&group=public&page=1&pageSize=20&size=small&filetype=json&license=all'>Kaggle.</a>");
+
+  //Width and height of margin
+  var w = 800;
+  var h = 800;
+
+  // make margins
+  var margin = { top: 20, right: 20, bottom: 20, left: 20};
+
+  //make the graph variables
+  var graphWidth = w - margin.right - margin.leftl;
+  var graphHeigth = h - margin.top - margin.bottom;
+  var bars = data.length;
+  var barPadding = 2;
+  var barWidth = ((graphWidth - barPadding * bars) / bars);
+
+  //define SVG
+  var svg = d3.select("body")
+              .append("svg")
+              .attr("width", w)
+              .attr("height", h);
+
+  //define rect
+  svg.selectAll("rect")
+      .data(data)  // <-- The answer is here!
+      .enter()
+      .append("rect")
+      .attr("class", "bar")
+      .attr("x", function(d, i) {
+      return i * barWidth + margin.left
+      })
+      .attr("y", function (d) {
+      return h - d["Happiness Score"] - margin.top
+      })
+      .attr("height", d["Happiness Score"])
+      .attr("width", barWidth)
+      .attr("fill", "teal");
+
+  //define scale and axis of chart
+  var xScale = d3.scale.ordinal()
+                  .domain([0, d3.max(data, function(d) { return d[0]; })])
+                  .range([0, w]);
+
+  var xAxis = d3.svg.axis()
+                .scale(xScale)
+                .orient("bottom");
+
+  var yScale = d3.scale.linear()
+                  .domain([0, d3.max(dataset, function(d) { return d[1]; })])
+                  .range([0, h]);
+
+  var yAxis = d3.svg.axis()
+                .scale(yScale)
+                .orient("left")
+                .ticks(20);
+
+  //Create x axis
+  svg.append("g")
+      .attr("class", "axis")
+      .attr("transform", "translate(" + padding + ",0)")
+      .call(xAxis);
+
+  //Create Y axis
+  svg.append("g")
+      .attr("class", "axis")
+      .attr("transform", "translate(" + padding + ",0)")
+      .call(yAxis);
+
+  //add text to axis
+  svg.selectAll("text")
+     .data(dataset)
+     .enter()
+     .append("text")
+     .attr("x", function(d, i) {
+           return d["Country"];
+      })
+      .attr("y", function(d) {
+           return "Rating on scale of 1-10";
+      });
+
+  //creating interactivity
+  svg.selectAll(".bar")
+    .data(data)
+  .enter().append("rect")
+    .attr("class", "bar")
+    .attr("x", function(d) { return x(d['Country']); })
+    .attr("width", x.rangeBand())
+    .attr("y", function(d) { return y(d["Happiness Score"]); })
+    .attr("height", function(d) { return height - y(d["Happiness Score"]); })
+    .on('mouseover', tip.show)
+    .on('mouseout', tip.hide)
 });
--- homework/week_3/convertCSV2JSON.py
+++ homework/week_3/convertCSV2JSON.py
@@ -4,10 +4,16 @@ import json
 jsonfile = open('2016_happiness_westerneurope.json', 'w')
 
 with open('2016_happiness_westerneurope.csv', 'r') as csv_file:
-    csv_reader = csv.DictReader(csv_file)
+    csv_reader = csv.DictReader(csv_file, delimiter=",")
+
+    jsonfile.write("{ \"data\": [")
+    jsonfile.write("\n")
 
     for line in csv_reader:
-        s = json.dumps([line])
+        s = json.dumps(line)
         jsonfile.write(s)
-        
+        jsonfile.write(",")
+        jsonfile.write("\n")
+
+    jsonfile.write("]")
 jsonfile.close()
